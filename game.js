fd4b9f088b43f2b63bf64f6c7320ca4ab3ad8cf8
Yauheniy Baihot <yauheniybaihot@gmail.com>
2020-01-19 23:07:37 +0300

    Final version

--- game.js
+++ game.js
@@ -6,169 +6,177 @@ const left = 'left';
 const right = 'right';
 
 const directions = [
-    {name: up, xChange: 0, yChange: -1},
-    {name: down, xChange: 0, yChange: 1},
-    {name: left, xChange: -1, yChange: 0},
-    {name: right, xChange: 1, yChange: 0},
+  { name: up, xChange: 0, yChange: -1 },
+  { name: down, xChange: 0, yChange: 1 },
+  { name: left, xChange: -1, yChange: 0 },
+  { name: right, xChange: 1, yChange: 0 },
 ]
 
 
 class Point {
-    #x;
-    #y;
+  #x;
+  #y;
 
-    constructor(x, y) {
-        if (!Number.isInteger(x)) {
-            throw ('x is not valid coordinate')
-        }
-
-        if (!Number.isInteger(y)) {
-            throw ('y is not valid coordinate')
-        }
-
-        this.#x = x;
-        this.#y = y;
+  constructor(x, y) {
+    if (!Number.isInteger(x)) {
+      throw ('x is not valid coordinate')
     }
 
-    equals(point) {
-        return this.#x === point.#x && this.#y === point.#y;
+    if (!Number.isInteger(y)) {
+      throw ('y is not valid coordinate')
     }
 
-    get x() {
-        return this.#x;
-    }
+    this.#x = x;
+    this.#y = y;
+  }
 
-    get y() {
-        return this.#y;
-    }
+  equals(point) {
+    return this.#x === point.#x && this.#y === point.#y;
+  }
+
+  get x() {
+    return this.#x;
+  }
+
+  get y() {
+    return this.#y;
+  }
 }
 
 class Game {
-    static get maxSpeedFactor() {
-        return maxSpeedFactor;
+  static get maxSpeedFactor() {
+    return maxSpeedFactor;
+  }
+
+  #food;
+
+  get food() {
+    return this.#food;
+  }
+
+  #previousDirection;
+
+  #snake = [];
+
+  get snake() {
+    return this.#snake;
+  }
+
+  #score = 0;
+
+  get score() {
+    return this.#score;
+  }
+
+  #speedFactor = 1;
+
+  get speedFactor() {
+    return this.#speedFactor;
+  }
+
+  #width;
+
+  get width() {
+    return this.#width;
+  };
+
+  #height;
+
+  get height() {
+    return this.#height;
+  };
+
+  #boundaries;
+
+  constructor(width, height, boundaries) {
+    if (!Number.isInteger(width) || width < 10) {
+      throw 'Incorrect width';
     }
 
-    #food;
-
-    get food() {
-        return this.#food;
+    if (!Number.isInteger(height) || height < 10) {
+      throw 'Incorrect height';
     }
 
-    #previousDirection;
+    this.#boundaries = !!boundaries;
+    this.#width = width;
+    this.#height = height
+    this.#previousDirection = directions.find(x => x.name === right);
 
-    #snake = [];
+    const halfWidth = Math.round(this.width / 2);
+    const halfHeight = Math.round(this.height / 2);
 
-    get snake() {
-        return this.#snake;
+    this.snake.push(new Point(halfWidth, halfHeight));
+    this.snake.push(new Point(halfWidth + 1, halfHeight));
+    this.snake.push(new Point(halfWidth + 2, halfHeight));
+
+    this.createFood();
+  }
+
+  move(directionName) {
+    let direction = directions.find(x => x.name === directionName) || this.#previousDirection;
+
+    // Check for imposable move (turn over)
+    if (direction.xChange * this.#previousDirection.xChange === -1 ||
+      direction.yChange * this.#previousDirection.yChange === -1) {
+      direction = this.#previousDirection;
     }
 
-    #score = 0;
+    this.#previousDirection = direction;
 
-    get score() {
-        return this.#score;
+    const newPosition = new Point(
+      (this.width + this.head.x + direction.xChange) % this.width,
+      (this.height + this.head.y + direction.yChange) % this.height);
+
+    // hit snake or wall (for boundaries mode) 
+    if (this.snake.some((x, index) => newPosition.equals(x) && index !== 0)
+      || (this.#boundaries
+        && (this.head.x + direction.xChange < 0
+          || this.head.x + direction.xChange >= this.width
+          || this.head.y + direction.yChange < 0
+          || this.head.y + direction.yChange >= this.height))) {
+      return false;
+    } else if (newPosition.equals(this.food)) {
+      this.snake.push(newPosition);
+      this.#score++;
+
+      if (this.score % 5 === 0 && this.speedFactor < Game.maxSpeedFactor) {
+        this.#speedFactor++;
+      }
+
+      this.createFood();
+    } else {
+      this.snake.shift();
+      this.snake.push(newPosition);
     }
 
-    // TODO: from 1 to 10;
-    #speedFactor = 1;
+    return true;
+  }
 
-    get speedFactor() {
-        return this.#speedFactor;
-    }
+  get head() {
+    return this.snake[this.snake.length - 1];
+  }
 
-    #width = 20;
-
-    get width() {
-        return this.#width;
-    };
-
-    #height = 20;
-
-    get height() {
-        return this.#height;
-    };
-
-    #boundaries;
-
-    // TODO:  check input params.
-    constructor(speedFactor = 1, boundaries) {
-        this.#boundaries = boundaries;
-        this.#speedFactor = speedFactor;
-        this.#previousDirection = directions.find(x => x.name === right);
-
-        const halfWidth = Math.round(this.width / 2);
-        const halfHeight = Math.round(this.height / 2);
-
-        this.snake.push(new Point(halfWidth, halfHeight));
-        this.snake.push(new Point(halfWidth + 1, halfHeight));
-        this.snake.push(new Point(halfWidth + 2, halfHeight));
-
-        this.createFood();
-
-    }
-
-    move(directionName) {
-        let direction = directions.find(x => x.name === directionName) || this.#previousDirection;
-
-        if (direction.xChange * this.#previousDirection.xChange === -1 ||
-            direction.yChange * this.#previousDirection.yChange === -1) {
-            direction = this.#previousDirection;
+  createFood() {
+    const emptyFields = [];
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        const point = new Point(i, j);
+        if (!this.snake.some((x) => x.equals(point))) {
+          emptyFields.push(point);
         }
-
-        this.#previousDirection = direction;
-
-        const newPosition = new Point(
-            (this.width + this.head.x + direction.xChange) % this.width,
-            (this.height + this.head.y + direction.yChange) % this.height);
-
-        if (this.snake.some((x, index) => newPosition.equals(x) && index !== 0)
-            || (this.#boundaries
-                && (this.head.x + direction.xChange < 0
-                    || this.head.x + direction.xChange >= this.width
-                    || this.head.y + direction.yChange < 0
-                    || this.head.y + direction.yChange >= this.height))) {
-            return false;
-        } else if (newPosition.equals(this.food)) {
-            this.snake.push(newPosition);
-            this.#score++;
-
-            if (this.score % 5 === 0 && this.speedFactor < Game.maxSpeedFactor) {
-                this.#speedFactor++;
-            }
-
-            this.createFood();
-        } else {
-            this.snake.shift();
-            this.snake.push(newPosition);
-        }
-
-        return true;
+      }
     }
 
-    get head() {
-        return this.snake[this.snake.length - 1];
-    }
+    const randomPointIndex = Math.floor(Math.random() * Math.floor(emptyFields.length - 1));
 
-    createFood() {
-        const emptyFields = [];
-        for (let i = 0; i < this.width; i++) {
-            for (let j = 0; j < this.height; j++) {
-                const point = new Point(i, j);
-                if (!this.snake.some((x) => x.equals(point))) {
-                    emptyFields.push(point);
-                }
-            }
-        }
-
-        const randomPointIndex = Math.floor(Math.random() * Math.floor(emptyFields.length - 1));
-
-        this.#food = emptyFields[randomPointIndex];
-    }
+    this.#food = emptyFields[randomPointIndex];
+  }
 }
 
 export {
-    Game,
-    up as UpDirection,
-    down as DownDirection,
-    left as LeftDirection,
-    right as RightDirection
+  Game,
+  up as UpDirection,
+  down as DownDirection,
+  left as LeftDirection,
+  right as RightDirection
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {Game, LeftDirection, DownDirection, RightDirection, UpDirection} from "./game.js";
+import { Game, LeftDirection, DownDirection, RightDirection, UpDirection } from "./game.js";
 
 let game;
 
@@ -7,102 +7,90 @@ const context = canvas.getContext('2d');
 const highScoreDiv = document.getElementById('high-score');
 const speedFactorDiv = document.getElementById('speed-factor');
 const newGameButton = document.getElementById('new-game');
-const speedFactorInput = document.getElementById('speed-factor-input');
 const boundariesInput = document.getElementById('boundaries-input');
 let previousUpdate = null;
 
 let cancel;
 
-let speedUp = false;
-
-
 const movesStack = [];
 
 const frames = [200, 150, 130, 115, 100, 90, 80, 60, 45, 30];
 
-// toDO: run animation every frame
 function processFrame(timestamp) {
-    cancel = window.requestAnimationFrame(processFrame);
-    const frame = timestamp - previousUpdate;
+  cancel = window.requestAnimationFrame(processFrame);
+  const frame = timestamp - previousUpdate;
 
-    if (frame < frames[game.speedFactor - 1] && previousUpdate != null) {
-        return;
-    }
-    previousUpdate = timestamp;
+  if (frame < frames[game.speedFactor - 1] && previousUpdate != null) {
+    return;
+  }
+  previousUpdate = timestamp;
 
-    if (!game.move(movesStack.shift())) {
-        alert('GameOver');
-        window.cancelAnimationFrame(cancel);
-    }
+  if (!game.move(movesStack.shift())) {
+    alert('GameOver');
+    window.cancelAnimationFrame(cancel);
+  }
 
 
-    draw();
+  draw();
 }
 
 function draw() {
-    context.save();
-    context.scale(canvas.width / game.width, canvas.height / game.height);
+  context.save();
+  context.scale(canvas.width / game.width, canvas.height / game.height);
 
-    context.clearRect(0, 0, game.width, game.height);
+  context.clearRect(0, 0, game.width, game.height);
 
-    context.lineWidth = 1 / 20;
+  context.lineWidth = 1 / 20;
 
-    for (let i = 0; i < game.snake.length; i++) {
+  for (let i = 0; i < game.snake.length; i++) {
 
-        if (game.snake[i] === game.head) {
-            context.strokeStyle = 'red';
-            context.fillStyle = 'red';
-        }
-
-        context.strokeRect(game.snake[i].x + 1 / 20, game.snake[i].y + 1 / 20, 18 / 20, 18 / 20);
-        context.fillRect(game.snake[i].x + 3 / 20, game.snake[i].y + 3 / 20, 14 / 20, 14 / 20)
+    if (game.snake[i] === game.head) {
+      context.strokeStyle = 'red';
+      context.fillStyle = 'red';
     }
 
-    context.strokeStyle = 'green';
-    context.fillStyle = 'green';
+    context.strokeRect(game.snake[i].x + 1 / 20, game.snake[i].y + 1 / 20, 18 / 20, 18 / 20);
+    context.fillRect(game.snake[i].x + 3 / 20, game.snake[i].y + 3 / 20, 14 / 20, 14 / 20)
+  }
 
-    context.strokeRect(game.food.x + 1 / 20, game.food.y + 1 / 20, 18 / 20, 18 / 20);
-    context.fillRect(game.food.x + 3 / 20, game.food.y + 3 / 20, 14 / 20, 14 / 20);
+  context.strokeStyle = 'green';
+  context.fillStyle = 'green';
 
-    context.restore();
+  context.strokeRect(game.food.x + 1 / 20, game.food.y + 1 / 20, 18 / 20, 18 / 20);
+  context.fillRect(game.food.x + 3 / 20, game.food.y + 3 / 20, 14 / 20, 14 / 20);
 
-    highScoreDiv.innerText = game.score;
-    speedFactorDiv.innerText = game.speedFactor;
+  context.restore();
+
+  highScoreDiv.innerText = game.score;
+  speedFactorDiv.innerText = game.speedFactor;
 }
 
 document.addEventListener('keydown', (event) => {
-    // up
-    switch (event.code) {
-        case 'Space':
-            speedUp = true;
-            break;
-        case 'KeyS':
-        case 'ArrowDown':
-            movesStack.push(DownDirection);
-            break;
-        case 'KeyW':
-        case 'ArrowUp':
-            movesStack.push(UpDirection);
-            break;
-        case 'KeyA':
-        case 'ArrowLeft':
-            movesStack.push(LeftDirection);
-            break;
-        case 'KeyD':
-        case 'ArrowRight':
-            movesStack.push(RightDirection);
-            break;
-    }
+  // up
+  switch (event.code) {
+    case 'Space':
+      speedUp = true;
+      break;
+    case 'KeyS':
+    case 'ArrowDown':
+      movesStack.push(DownDirection);
+      break;
+    case 'KeyW':
+    case 'ArrowUp':
+      movesStack.push(UpDirection);
+      break;
+    case 'KeyA':
+    case 'ArrowLeft':
+      movesStack.push(LeftDirection);
+      break;
+    case 'KeyD':
+    case 'ArrowRight':
+      movesStack.push(RightDirection);
+      break;
+  }
 });
 
-document.addEventListener('keyup', (event) => {
-    if (event.code === 'Space') {
-        speedUp = false;
-    }
-});
-
-newGameButton.addEventListener('click', (event) => {
-
-    game = new Game(speedFactorInput.value, boundariesInput.checked);
-    cancel = requestAnimationFrame(processFrame);
+newGameButton.addEventListener('click', () => {
+  game = new Game(20, 20, boundariesInput.checked);
+  cancel = requestAnimationFrame(processFrame);
 });
